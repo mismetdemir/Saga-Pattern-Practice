326a4d0ef2186e7e948646c5468e9e75d73f4ed8
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 11:38:33 +0300

    Added Order model

---
+++ SagaPatternPractice/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using SagaPatternPractice.Enums;
+
+namespace SagaPatternPractice.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
