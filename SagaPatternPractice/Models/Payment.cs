0a61c66e1688dc78e8652d8e8cbd0631987740b0
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 11:42:08 +0300

    Added Payment model

---
+++ SagaPatternPractice/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using SagaPatternPractice.Enums;
+
+namespace SagaPatternPractice.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
