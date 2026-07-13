da093765d4cc989971d64ba6f03070b508a1c190
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 11:59:42 +0300

    Connected to SQL server

---
+++ SagaPatternPractice/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SagaPatternPractice.Models;
+
+namespace SagaPatternPractice.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
