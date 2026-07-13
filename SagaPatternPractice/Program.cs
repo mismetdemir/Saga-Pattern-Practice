8e78d66c3af61169e33ed72b3828c3d1584c990e
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 15:17:52 +0300

    Added services to builder

--- SagaPatternPractice/Program.cs
+++ SagaPatternPractice/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SagaPatternPractice.Data;
+using SagaPatternPractice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,12 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<StockService>();
+builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<CargoService>();
+builder.Services.AddScoped<OrderSagaService>(); 
+
 
 var app = builder.Build();
 
