9672a4bfdd0ecfd6245a88ab3bb7fa963ae86bf8
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 13:00:33 +0300

    Created DTO for Order

---
+++ SagaPatternPractice/DTOs/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SagaPatternPractice.DTOs
+{
+    public class OrderDto
+    {
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+        [Range(1, 10)]
+        public int Quantity { get; set; }
+    }
+}
