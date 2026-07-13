9f5b8dfde1af6a0b7375afa67d66665fabc6dc78
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 16:13:54 +0300

    Created DTO for product creation

---
+++ SagaPatternPractice/DTOs/CreateProdcutDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SagaPatternPractice.DTOs
+{
+    public class CreateProdcutDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public int StockQuantity { get; set; }
+
+    }
+}
