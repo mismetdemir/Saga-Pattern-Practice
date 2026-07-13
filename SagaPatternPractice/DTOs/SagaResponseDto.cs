33fe58d21b996312db2bbbbd110a2cd92dcfaa22
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 15:00:35 +0300

    Created DTO for response log

---
+++ SagaPatternPractice/DTOs/SagaResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SagaPatternPractice.DTOs
+{
+    public class SagaResponseDto
+    {
+        public bool Success { get; set; }
+        public int OrderId { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public List<string> Steps { get; set; } = new List<string>();
+    }
+}
