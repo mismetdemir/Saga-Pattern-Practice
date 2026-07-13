c1fe82029f4d1ed66119e179a002e2999f13394a
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 11:36:26 +0300

    Added Product model

---
+++ SagaPatternPractice/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SagaPatternPractice.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        
+    }
+}
