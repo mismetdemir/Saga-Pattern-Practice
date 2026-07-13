1142c2693046a055fc282fb2e0c0e5582508316c
mismetdemir <mismetdemir@hotmail.com>
2026-07-13 13:42:09 +0300

    Added services for cargo process

---
+++ SagaPatternPractice/Services/CargoService.cs
@@ -0,0 +1,10 @@
+﻿namespace SagaPatternPractice.Services
+{
+    public class CargoService
+    {
+        public async Task<bool> PrepareCargoAsync(bool failed)
+        {
+            return !failed;
+        }
+    }
+}
